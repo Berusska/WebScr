e426dd05e0a9ce471d30f7e9a3f78dd966799390
Berusska <karasek.s54@gmail.com>
2023-03-07 21:22:03 +0100

    Zajištěno stahování, chyby neodladěny.

--- webscr.py
+++ webscr.py
@@ -2,11 +2,14 @@ from googlesearch import search
 import pandas
 from pathlib import Path
 import webbrowser
-import keyboard
+import requests
+
 
 xx = pandas.read_excel("../css/uniqZdr.xlsx")
 xx = xx.reset_index()
-x = xx.iloc[:5]
+x = xx.iloc[1:5]
+
+urls = []
 
 for index, row in x.iterrows():
     aut = str(row['Author']).replace(".", "")
@@ -20,14 +23,25 @@ for index, row in x.iterrows():
     
     ttl = str(row["Title"]).replace("[online", "")
     
-    query = b[0] + " " + ttl + " pdf"
+    query = str(b[0]).split(" ")[0] + " " + ttl + " pdf"
     
-    srch = search(query, tld="co.in", num=3, stop=10, pause=2)
+    srch = search(query, tld="co.in", num=3, stop = 3, pause=2)
         
     for url in srch:
-        webbrowser.open(url)
-    
+        if ".pdf" in str(url):
+            webbrowser.open(url)
+            
+            urls.append(url)
+            
+            nazev = url.split("/")[-1].split(".pdf")[0].replace("%", "")
+            cesta = Path("./downloaded/" + query + "_@_" + nazev + ".pdf")
+            response = requests.get(url)
+            cesta.write_bytes(response.content)
+
     print(query)
-    print("\t\tČekám na stisk klávesy abych mohl hledat další...")
-    keyboard.sleep()
+    print("\tČekám na stisk klávesy abych mohl hledat další...")
+    
+    input("\033[31m\tPress Enter to continue...\033[0m") #https://stackoverflow.com/questions/66206815/how-to-change-the-ouput-color-in-the-terminal-of-visual-code
+
+
     
