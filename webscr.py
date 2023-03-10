c5a3a24c3c7503b4644546e01ae909312ffa44d6
Berusska <karasek.s54@gmail.com>
2023-03-10 11:56:51 +0100

    Odladěno, jejiný error je 429

--- webscr.py
+++ webscr.py
@@ -8,13 +8,13 @@ import time
 
 xx = pandas.read_excel("./uniqZdr.xlsx")
 xx = xx.reset_index()
-x = xx.iloc[0:5]
+x = xx.iloc[0:50]
 
 df = pandas.DataFrame(columns=["o_Title","o_Author","Title", "Author", "Query","URLs", "Match", "Match_n"])
 
 urls = []
 
-lsrch = ['https://loschmidt.chemi.muni.cz/peg/wp-content/uploads/2013/01/chemlisty2010.pdf', 'https://dspace.cvut.cz/bitstream/handle/10467/75175/FBMI-DP-2016-Vobecka-Katerina-prace.pdf?sequence=-1&isAllowed=y', 'https://dspace.cvut.cz/bitstream/handle/10467/91403/FBMI-DP-2020-Vesela-Katerina-prace.pdf?sequence=-1&isAllowed=y', 'https://dk.upce.cz/bitstream/handle/10195/46323/Chladkova_Vyuziti%20Biosensoru_SS_2012.pdf?sequence=3&isAllowed=y', 'https://dk.upce.cz/bitstream/handle/10195/68479/PorizkovaP_Priprava_cholinesterazoveho_MP_2017.pdf?sequence=1&isAllowed=y', 'https://www.researchgate.net/profile/Pavla_Martinkova4/publication/323869415_Stanoveni_glykemie_pomoci_biosenzoru_soucasny_stav_a_trendy_ve_vyzkumu/links/5ab0c8f10f7e9b4897c237d9/Stanoveni-glykemie-pomoci-biosenzoru-soucasny-stav-a-trendy-ve-vyzkumu.pdf', 'https://theses.cz/id/owjtyx/16321490', 'https://core.ac.uk/download/pdf/161964838.pdf', 'http://chemicke-listy.cz/Bulletin/bulletin413/bulletin413.pdf', 'https://dspace.cuni.cz/bitstream/handle/20.500.11956/47744/150007322.pdf?sequence=1&isAllowed=y']
+# lsrch = ['https://loschmidt.chemi.muni.cz/peg/wp-content/uploads/2013/01/chemlisty2010.pdf', 'https://dspace.cvut.cz/bitstream/handle/10467/75175/FBMI-DP-2016-Vobecka-Katerina-prace.pdf?sequence=-1&isAllowed=y', 'https://dspace.cvut.cz/bitstream/handle/10467/91403/FBMI-DP-2020-Vesela-Katerina-prace.pdf?sequence=-1&isAllowed=y', 'https://dk.upce.cz/bitstream/handle/10195/46323/Chladkova_Vyuziti%20Biosensoru_SS_2012.pdf?sequence=3&isAllowed=y', 'https://dk.upce.cz/bitstream/handle/10195/68479/PorizkovaP_Priprava_cholinesterazoveho_MP_2017.pdf?sequence=1&isAllowed=y', 'https://www.researchgate.net/profile/Pavla_Martinkova4/publication/323869415_Stanoveni_glykemie_pomoci_biosenzoru_soucasny_stav_a_trendy_ve_vyzkumu/links/5ab0c8f10f7e9b4897c237d9/Stanoveni-glykemie-pomoci-biosenzoru-soucasny-stav-a-trendy-ve-vyzkumu.pdf', 'https://theses.cz/id/owjtyx/16321490', 'https://core.ac.uk/download/pdf/161964838.pdf', 'http://chemicke-listy.cz/Bulletin/bulletin413/bulletin413.pdf', 'https://dspace.cuni.cz/bitstream/handle/20.500.11956/47744/150007322.pdf?sequence=1&isAllowed=y']
 
 for index, row in x.iterrows():
     aut = str(row['Author']).replace(".", "")
@@ -31,8 +31,8 @@ for index, row in x.iterrows():
     query = str(b) + " " + ttl + " pdf"
     print("\033[34m" + query + "\033[0m")
     
-    # srch = search(query, tld="co.in", num=10, stop=10, pause=2)
-    # lsrch = list(srch)
+    srch = search(query, tld="co.in", num=10, stop=10, pause=2)
+    lsrch = list(srch)
     
     citac = 0
     match = 0
@@ -54,7 +54,7 @@ for index, row in x.iterrows():
             if input("\t\tZdá se že je to daný zdroj?\t") == 1:
                 nazev += "&&&"
                 match += 1
-                n = nazev
+                n = n + ", " + nazev
             
             cesta = Path("./downloaded/" + query + "_@_" + nazev + ".pdf")
             response = requests.get(url)
@@ -64,7 +64,7 @@ for index, row in x.iterrows():
     
     input("\033[31m\tČekám na stisk klávesy abych mohl hledat další...\033[0m") #https://stackoverflow.com/questions/66206815/how-to-change-the-ouput-color-in-the-terminal-of-visual-code
 
-
+# print(df.to_string())
 # https://pypi.org/project/websearch-python/
 
 
