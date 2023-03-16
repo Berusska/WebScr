43da3049705504c9a1d44f6936bc14ebb22d4da8
Berusska <karasek.s54@gmail.com>
2023-03-16 17:30:07 +0100

    Progress

--- Scrape1.py
+++ Scrape1.py
@@ -30,14 +30,14 @@ dfIn = pandas.read_excel("./uniqZdr.xlsx").reset_index().iloc[40:50]
 
 dfOut = pandas.DataFrame(columns=["o_Title","o_Author","Title", "Author", "Query","URLs", "Match", "Match_n"])
 
-primSlozka = Path("C:/Users/karas/Downloads")
+primSlozka = Path("C:/Users/Admin/Downloads")
 sekSlozka = Path("./downloaded")
 sekSlozka.mkdir(parents=True, exist_ok=True)
-veSlozce = set(sorted(primSlozka.iterdir())) 
+veSlozce = set(sorted(primSlozka.glob("*.pdf"))) 
 
 urls = []; querys = []
 
-pc.copy(""); predtim = ""
+pc.copy("0"); predtim = "0"
 
 for index, radek in dfIn.iterrows():
     
@@ -57,44 +57,53 @@ for index, radek in dfIn.iterrows():
     pocitac = 0
     match = 0
     # predtim = pc.paste() #TODO: nefunguje mi to s tím porovnáváním, resp neotevírá se mi ani okno a tudíž to pak nezná 
-    event = "" #TODO: nepomohlo
+    event = "" #FIXME: nepomohlo
     lsrch = []
     indikace_schranky = 0
-    indikace_stazeni = 0
     
     while True: #nyní stále kontroluj, uživatel vyhledává a stahuje ... #TODO: já nevím jak se to bude chovat - protože: jakmile bude ve schránce nová polozka, musí to čekat než bude nová položka i ve složce, až pak může vyskožit okno a PŘEJMENOVAT SE TO 
         schranka = pc.paste()
-        if predtim != schranka:
-            nazev = schranka.split("/")[-1].split(".pdf")[0].replace("%", "")
-
-            layout = [[sg.Text(f"Ve schránce nová url adresa.\n\tAktuální zdroj je:\n\t{schranka}\nNazev bude: {nazev}")],[sg.Button('Pouze validní', pad=((20,15),3)), sg.Button('Daný zdroj', pad=((20,15),3)), sg.Button('Nevalidní daný zdroj', pad=((20,15),3)), sg.Button("Dej další zdroj", pad=((20,15),3), button_color="red")]] #TODO: DALŠÍ button pro daný zdroj ale s nevalidním názvem
-
-            event, other = sg.Window('Choose an option', layout, keep_on_top=True).read(close=True) 
-            predtim = schranka           
-        if event == "":
-            pass
-        elif event == "Dej další zdroj":
-            break 
-        elif event == "Pouze validní":
-            nazev = query.replace(" pdf", "") + nazev + str(pocitac)
-            pocitac += pocitac
-        elif event == "Daný zdroj":
-            nazev += "&&&"
-            match += 1
-        elif event == "Nevalidní daný zdroj":
-            nazev = query.replace(" pdf", "") + str(pocitac) + "&&&"
         
-        if event in ["Dej další zdroj", "Pouze validní", "Daný zdroj"]:      
-            urls.append(schranka)
+        if indikace_schranky == 0:
+            if predtim != schranka:
+                predtim = schranka
+                indikace_schranky = 1   
+        elif indikace_schranky == 1:
+            veSlozce_actual = set(sorted(primSlozka.glob("*.pdf")))
+            newFile = veSlozce.symmetric_difference(veSlozce_actual)
+            if len(newFile) != 0:
+                nazevFile = list(newFile)[0].name
             
-            cesta = Path("./downloaded/" + query.replace(" pdf", "") + "_@_" + nazev + ".pdf")
-            response = requests.get(schranka)
-            if response.status_code == 200:
-                cesta.write_bytes(response.content)
-            
-            lsrch.append(schranka)
+                layout = [[sg.Text(f"Ve schránce nová url adresa a zároveň se objevil nový PDF soubor v primární složce.\n\tNový soubor: {nazevFile}\n\n\tAktuální schránka: {schranka}\n")],[sg.Button('Další zajimavý zdroj', pad=((20,15),3)), sg.Button('Daný zdroj', pad=((20,15),3)), sg.Button("Dej další zdroj", pad=((20,15),3), button_color="red")]]
+                
+                event, other = sg.Window('Stahování zdrojů pd DP', layout, keep_on_top=True).read(close=True) 
+                
+                if event == "":
+                    pass
+                elif event == "Dej další zdroj":
+                    break 
+                elif event == "Další zajimavý zdroj":
+                    nazev = query.replace(" pdf", "") + nazev + str(pocitac)
+                    pocitac += pocitac
+                elif event == "Daný zdroj":
+                    nazev += "&&&"
+                    match += 1
+                elif event == "Nevalidní daný zdroj":
+                    nazev = query.replace(" pdf", "") + str(pocitac) + "&&&"
+                
+                if event in ["Dej další zdroj", "Pouze validní", "Daný zdroj"]:      
+                    urls.append(schranka)
+                    
+                    #TODO: přesun a přejmenování
+                    
+                    lsrch.append(schranka)
+        else: pass
+
+        
+
     
     dfOut.loc[len(dfOut)] = [radek["Title"], radek['Author'], ttl, Auth, query, lsrch, match]
+    indikace_schranky = 0
     
     input("\033[31m\tČekám na stisk klávesy abych mohl hledat další...\033[0m") 
 
